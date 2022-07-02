313a983782bfb0e7fe12a5d4debf4e9186397a4d
Mateusz Chorosiński <chorosinski.mateusz@gmail.com>
2022-07-03 01:24:56 +0300

    Add 2 conditional statements - 'papier' & 'nozyce'

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-let playerMove = 'papier'
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2){
+  computerMove = 'papier';  
+} else if (randomNumber == 3){
+  computerMove = 'nozyce';  
+}
+
+printMessage('Mój ruch to: ' + computerMove);
